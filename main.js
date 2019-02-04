f06b737e683ae16c3d0a3fa35252f20e4a8821de
Charles Woodring <charles.s.woodring@gmail.com>
2019-02-04 15:36:52 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+// Your job is to write a function that accepts any band name as an argument.
+// The function will increment a global variable by one each time it is invoked, 
+// and return that number, and the band name concatenated together.
+
+let bandNumber = 1
+
+const takeNumber = function (bandName) {
+    // concat "bandNumber" "bandname"
+    // (bandNumber = bandNumber +=)
+}
+
+const scum = takeNumber("Galactic Scum")
+console.log(scum)  // This should print "1. Galactic Scum" in the console
+
+const under = takeNumber("Underdogs")
+console.log(under)  // This should print "2. Underdogs" in the console
