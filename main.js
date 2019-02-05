3300eb21103872dba8e2989ae284afd210f13417
Charles Woodring <charles.s.woodring@gmail.com>
2019-02-04 19:47:33 -0600

    working copy

--- main.js
+++ main.js
@@ -5,12 +5,17 @@
 let bandNumber = 1
 
 const takeNumber = function (bandName) {
-    // concat "bandNumber" "bandname"
-    // (bandNumber = bandNumber +=)
+    console.log(`${bandNumber}. ${bandName}`) // <-------- concat the number and name
+    bandNumber = bandNumber +1 // <-------- increment counter
 }
 
-const scum = takeNumber("Galactic Scum")
-console.log(scum)  // This should print "1. Galactic Scum" in the console
+// const scum = takeNumber("Galactic Scum")
+// console.log(scum)  // This should print "1. Galactic Scum" in the console
+takeNumber("Galactic Scum")
 
-const under = takeNumber("Underdogs")
-console.log(under)  // This should print "2. Underdogs" in the console
+// const under = takeNumber("Underdogs")
+// console.log(under)  // This should print "2. Underdogs" in the console
+takeNumber("Underdogs")
+
+// console.log(under)  // This should print "3. Compass Failure" in the console
+takeNumber("Compass Failure")
